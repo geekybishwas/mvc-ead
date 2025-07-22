7da2cb686a06d92200419775ad3c98e5258f1dc3
geekybishwas <techiebishwas@gmail.com>
2025-07-23 00:44:53 +0545

    chore: update package.json scripts and dependencies, add vercel.json, and modify vite.config.js for build output

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
 
 export default defineConfig({
     plugins: [
@@ -8,6 +7,9 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
     ],
-});
+    build: {
+        outDir: 'dist',
+        emptyOutDir: true,
+    },
+})
